afd987e256230d8e61b56d155d43eeb29a096c7e
Daniel Franklin <daniel@danielzfranklin.org>
2024-08-09 00:32:38 +0100

    Replace csv-write-stream with csv-stringify
    This reduces my memory usage from around 3GiB to around 2.5GiB.
    csv-write-stream hasn't been updated in around 8 years. I tested this
    produces identical output on a recent timetable.

--- src/gtfs/output/FileOutput.ts
+++ src/gtfs/output/FileOutput.ts
@@ -1,17 +1,15 @@
-import csvWriter = require('csv-write-stream');
 import * as fs from "fs";
 import {GTFSOutput} from "./GTFSOutput";
 import {Writable} from "stream";
+import { stringify } from "csv-stringify";
 
 export class FileOutput implements GTFSOutput {
-
   /**
    * Wrapper around file output library that returns a file as a WritableStream
    */
   public open(filename: string): Writable {
-    const writer = csvWriter();
+    const writer = stringify({ header: true });
     writer.pipe(fs.createWriteStream(filename));
-
     return writer;
   }
 
